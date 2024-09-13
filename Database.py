ab68aa04e1062f4f4e9b2859a4ef772d24d5b9f5
Cahandfo <Cahandfo@uark.edu>
2024-09-13 09:36:27 -0500

    updating Database app

--- Database.py
+++ Database.py
@@ -5,9 +5,16 @@ try:
         dbname = "photon",
         user = "student",
         password = "student",
-        host = "",
         port = "5432"
     )
+    
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * from players;")
+
+    record = cursor.fetchone()
+    print(record)
+    print("Trying.....")
 except Exception as error:
     print("Error connecting to Database:", error)
 
