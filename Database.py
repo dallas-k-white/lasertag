b9e5bf333a713750941d10f246e718cb1739f1c4
Cahandfo <Cahandfo@uark.edu>
2024-09-14 12:13:01 -0500

    adding player add method

--- Database.py
+++ Database.py
@@ -1,23 +1,61 @@
 import psycopg2
 
-try:
-    conn = psycopg2.connect(
-        dbname = "photon",
-        user = "student",
-        password = "student",
-        port = "5432"
-    )
-    
-    cursor = conn.cursor()
+def connect_db():
+    try:
+        conn = psycopg2.connect(
+            dbname = "photon",
+            user = "student",
+            password = "student",
+            port = "5432"
+        )
+        print ("Database Connection Established")
+        return conn
+    except Exception as error:
+        print("Error connecting to Database:", error)
 
-    cursor.execute("SELECT * from players;")
 
-    record = cursor.fetchone()
-    print(record)
-    print("Trying.....")
-except Exception as error:
-    print("Error connecting to Database:", error)
+def add_player(name: str):
+    conn = connect_db()
+    try:
+        cursor = conn.cursor()
 
-finally:
-    if conn:
+        playerID = get_id_increment()
+
+        cursor.execute("""
+        INSERT INTO players (id, codename) values (%s, %s);
+        """,
+        (playerID, name))
+
+        print("Player " + name + "successfully added")
+
+        conn.commit()
         conn.close()
+        cursor.close()
+
+    except Exception as error:
+        print("Error:", error)
+
+
+def get_id_increment() -> int:
+    conn = connect_db()
+    try:
+        cursor = conn.cursor()
+
+        cursor.execute("""
+        SELECT id FROM players ORDER BY id DESC LIMIT 1;
+        """)
+        entry = cursor.fetchone()
+
+        cursor.close()
+        conn.close()
+
+        lastID = entry[0]
+        print(lastID)
+        lastID += 1
+        return lastID
+
+    except Exception as error:
+        print("Error:", error)
+
+add_player("Bob")
+add_player("Joe")
