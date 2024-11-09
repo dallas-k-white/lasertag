02ccebcd5dd3f6d90bc6a64601deea9f881bdaf8
Caden Ross <168718297+downzydwarf@users.noreply.github.com>
2024-11-09 16:07:20 -0600

    Update PlayerAction.py

--- PlayerAction.py
+++ PlayerAction.py
@@ -1,7 +1,36 @@
 import tkinter as tk
 from tkinter import ttk
 from Database import get_players, find_player
+import PlayerEdit
 import udp_handler 
+
+
+def update_timer(label, remaining_time, disable_ui_func, return_button_frame):
+    minutes, seconds = divmod(remaining_time, 60)
+    time_str = f"{minutes:02}:{seconds:02}"
+    label.config(text=time_str)
+
+    if remaining_time <= 0:
+        label.config(text="00:00")
+        disable_ui_func()
+        show_return_button(return_button_frame)
+    else:
+        label.after(1000, update_timer, label, remaining_time - 1, disable_ui_func, return_button_frame)
+
+def disable_ui(root):
+    for widget in root.winfo_children():
+        widget.config(state=tk.DISABLED)
+
+def show_return_button(return_button_frame, root):
+    return_button = tk.Button(return_button_frame, text="Return to Player Edit Screen", font=("Arial", 14), command=go_to_player_edit_screen)
+    return_button.pack(pady=10)
+
+def go_to_player_edit_screen():
+    global main_frame
+
+    main_frame.pack_forget()
+
+    PlayerEdit.build(root)
 
 def get_player_action(team1_entered_players, team2_entered_players):
 
@@ -48,6 +77,17 @@ def get_player_action(team1_entered_players, team2_entered_players):
         log_listbox = tk.Listbox(action_log_frame, width=50, height=20)
         log_listbox.grid(row=1, column=0, padx=10, pady=10)
 
+        # Timer Display
+        timer_label = tk.Label(root, text="06:00", font=("Arial", 14), fg="black")
+        timer_label.place(relx=0.95, rely=0.95, anchor='se')
+
+        remaining_time = 6 * 60
+
+        return_button_frame = tk.Frame(root)
+        update_timer(timer_label, remaining_time, disable_ui, return_button_frame)
+
+        return_button_frame.place(relx=0.95, rely=0.90, anchor='se')
+
         #play_track()
 
         # For testing purposes
@@ -56,13 +96,14 @@ def get_player_action(team1_entered_players, team2_entered_players):
     
     return build_player_action
 
-# def play_track():
-#     tracks = ["Tracks/Track01.mp3", "Tracks/Track02.mp3", "Tracks/Track03.mp3", "Tracks/Track04.mp3", "Tracks/Track05.mp3",
-#               "Tracks/Track06.mp3", "Tracks/Track07.mp3", "Tracks/Track08.mp3"]
-#     t = random.choice(tracks)
-#     pygame.mixer.music.load(t)
-#     print(t)
-#     pygame.mixer.music.play(loops=0)
+#def play_track():
+    #tracks = ["Tracks/Track01.mp3", "Tracks/Track02.mp3", "Tracks/Track03.mp3", "Tracks/Track04.mp3", "Tracks/Track05.mp3",
+    #        "Tracks/Track06.mp3", "Tracks/Track07.mp3", "Tracks/Track08.mp3"]
+    #t = random.choice(tracks)
+    #pygame.mixer.music.load(t)
+    #print(t)
+    #pygame.mixer.music.play(loops=0)
+
 
 
 
